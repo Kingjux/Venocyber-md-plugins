179badaead40afa42446cc6146f60d022897a08d
Kingjux <154667110+Kingjux@users.noreply.github.com>
2024-03-17 17:38:27 +0300

    Create listPlugin.js

---
+++ plugins/listPlugin.js
@@ -0,0 +1,34 @@
+{          
+    "external": [        
+        {  "name": "AntiDelete","url": "https://raw.githubusercontent.com/Giftedmaurice/gifted-bot-md-plugins/main/plugins/delete.js"   },
+        {  "name": "AutoReact","url": "https://raw.githubusercontent.com/Giftedmaurice/gifted-bot-md-plugins/plugins/areact.js"   },  
+        {  "name": "AntiCall","url": "https://raw.githubusercontent.com/Giftedmaurice/gifted-bot-md-plugins/main/plugins/anticall.js"   },
+        {  "name": "AntiVV","url": "https://raw.githubusercontent.com/Giftedmaurice/gifted-bot-md-plugins/main/plugins/antivv.js"   },
+        {  "name": "DelSpam", "url": "https://raw.githubusercontent.com/Giftedmaurice/gifted-bot-md-plugins/main/plugins/delspam.js"   }, 
+        {  "name": "FakeReply","url": "https://raw.githubusercontent.com/Giftedmaurice/gifted-bot-md-plugins/main/plugins/fakereply.js"   }
+     ],
+    "result": [
+        {  "name": "live","url": "https://raw.githubusercontent.com/Giftedmaurice/gifted-bot-md-plugins/main/plugins/live.js"   }, 
+        {  "name": "teddy","url": "https://raw.githubusercontent.com/Giftedmaurice/gifted-bot-md-plugins/main/plugins/teddy.js"   },         
+        {  "name": "intro","url": "https://raw.githubusercontent.com/Giftedmaurice/gifted-bot-md-plugins/main/plugins/intro.js"   },
+        {  "name": "getall","url": "https://raw.githubusercontent.com/Giftedmaurice/gifted-bot-md-plugins/main/plugins/getall.js"   },
+        {  "name": "hack", "url": "https://raw.githubusercontent.com/Giftedmaurice/gifted-bot-md-plugins/main/plugins/hack.js"   }, 
+        {  "name": "ReactionPack","url": "https://gist.github.com/Giftedmaurice/34190d68ff50400345e00a7ccfb9294f"   },
+        {  "name": "gifsearch","url": "https://raw.githubusercontent.com/Giftedmaurice/gifted-bot-md-plugins/main/plugins/gifsearch.js"   },
+        {  "name": "stickersearch","url": "https://raw.githubusercontent.com/Giftedmaurice/gifted-bot-md-plugins/main/plugins/stickersearch.js"   },  
+        {  "name": "notes","url": "https://raw.githubusercontent.com/Giftedmaurice/gifted-bot-md-plugins/main/plugins/notes.js"   },   
+        {  "name": "levelup","url": "https://raw.githubusercontent.com/Giftedmaurice/gifted-bot-md-plugins/main/plugins/rank.js"   },
+        {  "name": "xmedia","url": "https://raw.githubusercontent.com/Giftedmaurice/gifted-bot-md-plugins/main/plugins/xmedia.js"   },
+        {  "name": "audio","url": "https://raw.githubusercontent.com/Giftedmaurice/gifted-bot-md-plugins/main/plugins/audio.js"   },
+        {  "name": "logo","url": "https://raw.githubusercontent.com/Giftedmaurice/gifted-bot-md-plugins/main/plugins/logo.js"   },
+        {  "name": "cat","url": "https://raw.githubusercontent.com/Giftedmaurice/gifted-bot-md-plugins/main/plugins/cat.js"   },
+        {  "name": "dog","url": "https://raw.githubusercontent.com/Giftedmaurice/gifted-bot-md-plugins/main/plugins/dog.js"   }
+    ]
+
+
+
+
+
+
+
+}
