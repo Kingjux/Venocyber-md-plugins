cd41f14ddd0beed9841510e3bf13b6ad0ce9ccab
Kingjux <154667110+Kingjux@users.noreply.github.com>
2024-03-17 17:18:21 +0300

    Create waunbann.js

---
+++ plugins/waunbann.js
@@ -0,0 +1,89 @@
+/**
+CURRENTLY RUNNING ON BETA VERSION!!
+*
+   * @project_name : gifted-md
+   * @author : Gifted Tech <https://github.com/kingjux>
+   * @youtube : https://www.youtube.com/@JASTINMTEWA-vn9pl
+   * @infoription : Venocyber-Md ,A Multi-functional whatsapp user bot.
+   * @version 1.0.0 
+*
+   * Licensed under the  GPL-3.0 License;
+* 
+   * ┌┤Created By Venocyber Tech.
+   * © 2024 Venocyber-Md ✭ ⛥.
+   * plugin date : 17/3/2024
+* 
+   * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+   * SOFTWARE.
+**/
+
+
+
+
+
+
+
+const {
+    smd,
+    prefix, 
+    Config ,
+    sleep
+     } = require('../lib')
+
+
+
+
+
+
+
+smd({
+    cmdname: "waunbann",    
+    type: "fun",    
+    info: "whatsapp unbann",    
+    filename: __filename,
+
+},
+
+async(citel) => {    
+await citel.send("Almour-Unbann Bot Checking Phone Number")   
+await sleep(2000)    
+await citel.send(" Loading █ 10%")    
+await sleep(1000)    
+await citel.send(" Loading █ █ 20%")    
+await sleep(1000)    
+await citel.send(" Loading █ █ █ 30%")    
+await sleep(1000)    
+await citel.send(" Loading █ █ █ █ 40%")    
+await sleep(2000)    
+await citel.send(" Loading █ █ █ █ █ 50%")    
+await sleep(1000)    
+await citel.send(" Processing █ █ █ █ █ █ 60%")    
+await sleep(1000)    
+await citel.send(" Processing █ █ █ █ █ █ █ 70%")    
+await sleep(1000)    
+await citel.send(" Processing █ █ █ █ █ █ █ █ 80%")    
+await sleep(1000)    
+await citel.send(" Processing █ █ █ █ █ █ █ █ █ 90%")    
+await sleep(2000)    
+await citel.send(" Phone Number Checking Complete █ █ █ █ █ █ █ █ █ █ 100%")    
+await sleep(1000)    
+await citel.send("Phone Number Validation Process.. \n Conecting to the Official Whatsapp Unbann Server ")    
+await sleep(2000)    
+await citel.send("Phone Number and Device Info Successfully Connected to Server... \n Acquiring All Necessary Data to Unbann...")    
+await sleep(2000)    
+await citel.send("Data Downloaded from Server 100% Completed \n Specifying the Env. Variables to start Unbanning...")
+await sleep(2000)    
+await citel.send(" PHONE NUMBER UNBANN OFFICIALLY STARTED ")    
+await sleep(2000)    
+await citel.send(" YOUR WHATSAPP NUMBER UNBANN IS CONNECTED AND PROCESSING...")     
+await sleep(1000)
+    return await citel.send('Please be Patient. Will Automatically Update About the ban Issue Within 24Hrs - 48Hrs. Powered by Almour-Unbann Bot');
+
+}
+
+)
