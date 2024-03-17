325ca310fa8ffc8ed03ceaf4890deee84e57c5b1
Kingjux <154667110+Kingjux@users.noreply.github.com>
2024-03-17 17:44:56 +0300

    Create hack.js

---
+++ plugins/hack.js
@@ -0,0 +1,92 @@
+/**
+CURRENTLY RUNNING ON BETA VERSION!!
+*
+   * @project_name : Venocyber-md
+   * @author : Venocyber Tech <https://github.com/kingjux>
+   * @youtube : https://www.youtube.com/@JASTINMTEWA-vn9pl
+   * @infoription : Gifted-Md ,A Multi-functional whatsapp user bot.
+   * @version 1.2.6 
+*
+   * Licensed under the  GPL-3.0 License;
+* 
+   * ┌┤Created By Venocyber Tech.
+   * © 2024 Venocyber-Md ✭ ⛥.
+   * plugin date : 17/3/2024
+* 
+   * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+   * SOFTWARE.
+**/
+
+
+
+
+
+
+
+const {
+    smd,
+    prefix, 
+    Config ,
+    sleep
+     } = require('../lib')
+
+
+
+
+
+
+
+smd({
+    cmdname: "hack",    
+    type: "fun",    
+    info: "hacking prank",    
+    filename: __filename,
+
+},
+
+async(citel) => {    
+await citel.send("Venocyber-Md Injecting Malware")   
+await sleep(2000)    
+await citel.send(" █ 10%")    
+await sleep(1000)    
+await citel.send(" █ █ 20%")    
+await sleep(1000)    
+await citel.send(" █ █ █ 30%")    
+await sleep(1000)    
+await citel.send(" █ █ █ █ 40%")    
+await sleep(1000)    
+await citel.send(" █ █ █ █ █ 50%")    
+await sleep(1000)    
+await citel.send(" █ █ █ █ █ █ 60%")    
+await sleep(1000)    
+await citel.send(" █ █ █ █ █ █ █ 70%")    
+await sleep(1000)    
+await citel.send(" █ █ █ █ █ █ █ █ 80%")    
+await sleep(1000)    
+await citel.send(" █ █ █ █ █ █ █ █ █ 90%")    
+await sleep(1000)    
+await citel.send(" █ █ █ █ █ █ █ █ █ █ 100%")    
+await sleep(1000)    
+await citel.send("System Hijacking in Process.. \n Conecting to the Server Error to Find 404 ")    
+await sleep(1000)    
+await citel.send("Device Successfully Connected... \n Receiving All Data...")    
+await sleep(1000)    
+await citel.send("Data Hijacked from Device 100% Completed \n Killing all Evidence Killing all Malwares...")
+await sleep(1000)    
+await citel.send(" SYSTEM HACKING COMPLETED BY VENOCYBER MD ")    
+await sleep(2000)    
+await citel.send(" VENOCYBER MD SENDING LOG DOCUMENTS NOW...")    
+await sleep(1000)
+await citel.send("VENOCYBER MD SUCCESSFULLY SENT DATA AND Connection has Been Established")    
+await sleep(1000)
+
+    return await citel.send('BACKLOGS CLEARED AND NO EVIDENCE LEFT');
+
+}
+
+)
