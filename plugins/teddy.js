3b49034ccee0ac662dcba2d5ed4cd68af6967aca
Kingjux <154667110+Kingjux@users.noreply.github.com>
2024-03-17 17:19:35 +0300

    Create teddy.js

---
+++ plugins/teddy.js
@@ -0,0 +1,77 @@
+/**
+
+//══════════════════════════════════════════════════════════════════════════════════════════════════════//
+//                                                                                                      //
+//                                ＷＨＡＴＳＡＰＰ ＢＯＴ－ＭＤ ＢＥＴＡ                                   //
+//                                                                                                      // 
+//                                         Ｖ：1．2．2                                                   // 
+//                                                                                                      // 
+//            ███████╗██╗   ██╗██╗  ██╗ █████╗ ██╗██╗         ███╗   ███╗██████╗                        //
+//            ██╔════╝██║   ██║██║  ██║██╔══██╗██║██║         ████╗ ████║██╔══██╗                       //
+//            ███████╗██║   ██║███████║███████║██║██║         ██╔████╔██║██║  ██║                       //
+//            ╚════██║██║   ██║██╔══██║██╔══██║██║██║         ██║╚██╔╝██║██║  ██║                       //
+//            ███████║╚██████╔╝██║  ██║██║  ██║██║███████╗    ██║ ╚═╝ ██║██████╔╝                       //
+//            ╚══════╝ ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝╚══════╝    ╚═╝     ╚═╝╚═════╝                        //
+//                                                                                                      //
+//                                                                                                      //
+//                                                                                                      //
+//══════════════════════════════════════════════════════════════════════════════════════════════════════//
+
+CURRENTLY RUNNING ON BETA VERSION!!
+*
+   * @project_name : Suhail-Md
+   * @author : Suhail <https://github.com/SuhailTechInfo>
+   * @youtube : https://www.youtube.com/c/@SuhailTechInfo
+   * @infoription : Suhail-Md ,A Multi-functional whatsapp user bot.
+   * @version 1.2.6 
+*
+   * Licensed under the  GPL-3.0 License;
+* 
+   * ┌┤Created By Suhail Tech Info.
+   * © 2023 Suhail-Md ✭ ⛥.
+   * plugin date : 18/12/2023
+* 
+   * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+   * SOFTWARE.
+**/
+
+
+
+
+
+
+const {
+    smd,
+    prefix, 
+    Config ,
+    sleep
+    } = require('../lib'),teddyM = {"smd" : "suhail" }
+
+
+
+
+
+smd({
+    cmdname: "teddy",    
+    type: "fun",    
+    info: "cute teddy",   
+    on: "text" ,
+    filename: __filename,
+},async(citel,match , {smd}) => {
+  let isteddy = smd ==="teddy"?true : citel.isPublic && match.toLowerCase().includes("teddy") ? true : ""       
+      if (isteddy && !teddyM[citel.id]) {
+      teddyM[citel.id] =true;
+      let teddy = ['❤', '💕', '😻', '🧡', '💛', '💚', '💙', '💜', '🖤', '❣', '💞', '💓', '💗', '💖', '💘', '💝', '💟', '♥', '💌', '🙂', '🤗', '😌', '😉', '🤗', '😊', '🎊', '🎉', '🎁', '🎈']
+      const { key } = await citel.reply( `(\\_/)\n( •.•)\n/>🤍`)
+      for (let i = 0; i < teddy.length; i++) {
+        await sleep(500);
+        await citel.reply(`(\\_/)\n( •.•)\n/>${teddy[i]}`, { edit: key })             
+      } 
+    }
+
+})
